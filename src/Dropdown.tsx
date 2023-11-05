638f0c7eaa8631154b24c2c2aa0b27bdf35402e7
bsavanachai <bhanutas.savanachai@agoda.com>
2023-11-05 10:37:47 +0700

    modified dropdown

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,33 +1,48 @@
-import React, {FC} from 'react';
+// CustomDropdown.tsx
+import React, { useState, useRef, useEffect, FC } from 'react';
 
 interface DropdownProps {
     options: string[];
     onSelect: (selectedOption: string) => void;
 }
 
-const Dropdown: FC<DropdownProps> = ({options, onSelect}) => {
-    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        onSelect(e.target.value);
+const Dropdown: FC<DropdownProps> = ({ options, onSelect }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, [dropdownRef]);
+
+    const handleSelect = (option: string) => {
+        setSelectedOption(option);
+        onSelect(option);
+        setIsOpen(false);
     };
 
     return (
-        <select
-            onChange={handleSelect}
-            style={{
-                height: '25px',
-                width: '80px',
-                alignSelf: 'center',
-                right: '0',
-                position: 'absolute',
-                borderRadius: '5px',
-                border: 'none',
-                fontSize: '11px'
-            }}
-        >
-            {options.map((option, index) =>
-                <option key={index} value={option} >{option}</option>
+        <div className="dropdown" ref={dropdownRef}>
+            <button className="dropdown-toggle" onClick={() => setIsOpen(!isOpen)}>
+                {selectedOption} <span>&#9660;</span>
+            </button>
+            {isOpen && (
+                <ul className="dropdown-menu">
+                    {options.map((option, index) => (
+                        <li key={index} onClick={() => handleSelect(option)}>
+                            {option}
+                        </li>
+                    ))}
+                </ul>
             )}
-        </select>
+        </div>
     );
 };
 
